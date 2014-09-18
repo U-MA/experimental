7a887b275ddef663032a61a4110517cccb720d28
U-MA <g.0224incident@gmail.com>
2014-09-18 16:18:04 +0900

    指定した深さまで見れるようにした

--- tools/node_dump.h
+++ tools/node_dump.h
@@ -5,24 +5,26 @@ namespace VrpSolver {
     namespace tools {
 
         namespace detail {
-            void node_dump_impl(MctNode *node, int level);
+            void node_dump_impl(MctNode *node, int depth, int max_depth);
         }
 
         // MctNodeから成る木構造をダンプする
-        void node_dump(MctNode &root) {
-            detail::node_dump_impl(&root, 0);
+        // max_depthで何段目までなのかを指定する.
+        // 1であればROOTとその子まで. -1は全てを表示
+        void node_dump(MctNode &root, int max_depth = -1) {
+            detail::node_dump_impl(&root, 0, max_depth);
         }
 
         namespace detail {
-            void node_dump_impl(MctNode *node, int level)
+            void node_dump_impl(MctNode *node, int depth, int max_depth)
             {
                 char tab[80] = {};
-                for (int i=0; i < 4*level; ++i) {
+                for (int i=0; i < 4*depth; ++i) {
                     tab[i] = ' ';
                 }
 
                 printf("%snode_id:", tab);
-                if (level == 0) { // root
+                if (depth == 0) { // root
                     printf("ROOT\n");
                 } else {          // others
                     printf("%d\n", node->CustomerId());
@@ -30,9 +32,11 @@ namespace VrpSolver {
                 printf("%s  count:%3d,", tab, node->Count());
                 printf("num_childs:%3d\n", node->ChildSize());
                 if (node->ChildSize() != 0) {
-                    printf("%s  child:\n", tab);
-                    for (int i=0; i < node->ChildSize(); ++i) {
-                        node_dump_impl(node->Child(i), level+1);
+                    if (depth != max_depth) {
+                        printf("%s  child:\n", tab);
+                        for (int i=0; i < node->ChildSize(); ++i) {
+                            node_dump_impl(node->Child(i), depth+1, max_depth);
+                        }
                     }
                 }
             }
