db6fdff0f30095abebdf516de2a269d5efa8d623
U-MA <g.0224incident@gmail.com>
2014-09-22 11:20:51 +0900

    デバッグ用のコンパイルスクリプト

---
+++ compile_debug.sh
@@ -0,0 +1,9 @@
+gcc -g -Iinclude -Isymphony -Itools\
+    -D VRPDEBUG \
+    src/mt19937ar.cpp \
+    src/main.cpp \
+    src/host_vrp.cpp src/mct_node.cpp src/mct_selector.cpp \
+    src/solution.cpp src/solution_helper.cpp src/vehicle.cpp \
+    src/simulator.cpp \
+    symphony/compute_cost.c symphony/vrp_io.c symphony/vrp_macros.c \
+    -lstdc++
