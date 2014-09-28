b721652698aeca1949e10673f64692811058f610
U-MA <g.0224incident@gmail.com>
2014-09-28 12:36:03 +0900

    solution_helper.cppを削除したための変更

--- compile_debug.sh
+++ compile_debug.sh
@@ -3,7 +3,7 @@ gcc -g -Iinclude -Isymphony -Itools\
     src/mt19937ar.cpp \
     src/main.cpp \
     src/host_vrp.cpp src/mct_node.cpp src/mct_selector.cpp \
-    src/solution.cpp src/solution_helper.cpp src/vehicle.cpp \
+    src/solution.cpp src/vehicle.cpp \
     src/simulator.cpp \
     symphony/compute_cost.c symphony/vrp_io.c symphony/vrp_macros.c \
     -lstdc++
